efe8b12cb4c89a98087b69926c84c6e7e85f015c
saranbabu29 <sharanraj982@gmail.com>
2022-08-07 15:51:30 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CouponNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int coupon=0;
+            int number;
+            int temp=0;
+            Console.WriteLine("How many Coupons you want");
+            number = int.Parse(Console.ReadLine());
+            temp = coupon;
+            for (int i = 1; i <= number; i++)
+            {
+                Random random = new Random();
+                coupon = random.Next(100000,999999 );
+                
+                    if (coupon != temp)
+                    {
+                        Console.WriteLine(coupon);
+                    }
+            }
+        }
+    }
+}
